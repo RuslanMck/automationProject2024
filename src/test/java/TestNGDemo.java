4d9465db1c4f54062f2ccbe51231770cb7470de7
Ruslan Marchuk <mckruslan@gmail.com>
2024-08-11 13:35:18 +0200

    classwork 13 is done

--- src/test/java/TestNGDemo.java
+++ src/test/java/TestNGDemo.java
@@ -4,40 +4,116 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 import java.util.List;
 
 public class TestNGDemo {
 
+    private WebDriver driver;
+
+    private WebElement cookieBlock;
+    private WebElement acceptButton;
+    private WebElement firstNameInput;
+    private WebElement lastNameInput;
+    private WebElement emailInput;
+    private WebElement messageInput;
+
+
     private String baseUrl = "https://soliterata.com/practice-form/";
+    private By cookieLocator = By.xpath("//div[@data-elementor-id=\"4540\"]");
+    private By cookieAcceptLocator = By.xpath("//*[text()='Accept']");
+    private By firstNameInputLocator = By.xpath("//label[normalize-space(text())='First Name']" +
+            "/following-sibling::input");
+    private By lsatNameInputLocator = By.xpath("//label[normalize-space(text())='Last Name']" +
+            "/following-sibling::input");
+    private By emailInputLocator = By.xpath("//input[@id='form-field-email']");
+    private By messageInputLocator = By.xpath("//textarea[@id='form-field-message']");
+    private By genderSelectorLocator = By.xpath("//span[@class='elementor-field-option']");
 
-    @Test
-    public void inputTests(){
-        System.setProperty("selenium.chrome.driver", "path/to/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    private String firstName = "Bob";
+    private String message = "Test message 123";
+    private String lastName = "Obama";
+    private String email = "bob.obama@test.com";
+    private List<WebElement> genderSelector;
 
-        driver.get(baseUrl);
-
-        By cookieLocator = By.xpath("//div[@data-elementor-id=\"4540\"]");
-        WebElement cookieBlock = driver.findElement(cookieLocator);
-
-        By cookieAcceptLocator = By.xpath("//*[text()='Accept']");
-        WebElement acceptButton = driver.findElement(cookieAcceptLocator);
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    private void acceptCookies(){
+        cookieBlock = driver.findElement(cookieLocator);
+        acceptButton = driver.findElement(cookieAcceptLocator);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         if(cookieBlock.isDisplayed()){
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Accept']")));
             acceptButton.click();
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[text()='Accept']")));
         }
+    }
 
-        By inputsLocators = By.xpath("//input[@type='text']");
-        List<WebElement> inputFields = driver.findElements(inputsLocators);
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("selenium.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
 
-        inputFields.get(1).sendKeys("My sent key");
+        acceptCookies();
 
-        //1:18:15 / 2:24:07
+        firstNameInput = driver.findElement(firstNameInputLocator);
+        lastNameInput = driver.findElement(lsatNameInputLocator);
+        emailInput = driver.findElement(emailInputLocator);
+        messageInput = driver.findElement(messageInputLocator);
+        genderSelector = driver.findElements(genderSelectorLocator);
+    }
+
+    @Test
+    public void firstNameInputTests(){
+
+        firstNameInput.sendKeys(firstName);
+
+        Assert.assertEquals(firstNameInput.getAttribute("value"), firstName, "First name is not equals to expected " +
+                "result");
+    }
+
+    @Test
+    public void lastNameInputTest(){
+        lastNameInput.sendKeys(lastName);
+
+        Assert.assertTrue(ExpectedConditions.attributeToBe(lastNameInput, "value", lastName).apply(driver));
+    }
+
+    @Test
+    public void emailInputTest(){
+        emailInput.sendKeys(email);
+
+        Assert.assertTrue(ExpectedConditions.attributeToBe(emailInput, "value", email).apply(driver));
+    }
+
+    @Test
+    public void messageInputTest(){
+        messageInput.sendKeys(message);
+
+        Assert.assertEquals(messageInput.getAttribute("value"), message);
+
+        messageInput.clear();
+
+        /**
+         * Check that input field is empty
+         */
+        Assert.assertTrue(ExpectedConditions.attributeToBe(messageInput, "valur", "").apply(driver));
+    }
+
+    @Test
+    public void genderSelectorTest(){
+
+        for(WebElement e : genderSelector){
+            System.out.println(e.isSelected());
+            Assert.assertFalse(e.isSelected());
+        }
+    }
+
+    @AfterClass
+    public void testDown(){
+        driver.close();
     }
 }
